61e62d1456eb92540c1c48c4c87f3d653261c11c
Yeswanth Kumar <PGP32174@iiml.ac.in>
2018-03-30 20:30:38 +0530

    insital commit

--- static/custom.js
+++ static/custom.js
@@ -125,3 +125,77 @@ $(document).ready(function () {
         search();
     });
 });
+
+
+
+
+$(document).ready(function(e) {
+
+	var startX, curX, startY, curY; // Variables
+	var newXScroll, newYScroll, genXScroll; // More Variables!
+
+	// Change the height of the sidebar, as well as a few things to do with the main content area, so the user
+	// can actually scroll in the content area.
+	// function sideBarHeight() {
+    //
+	// 	var docHeight = $(document).height();
+	// 	var winHeight = $(window).height();
+    //
+	// 	$('.slide-in').height(winHeight);
+	// 	$('#main-container').height(winHeight);
+	// 	$('#sub-container').height($('#sub-container').height());
+	// }
+    //
+	// sideBarHeight();
+
+	var outIn = 'in';
+
+	Hammer(document.getElementById('cont')).on('swiperight', function(e) {
+/*			$('.slide-in').toggleClass('on');
+			$('#main-container').toggleClass('on');
+			outIn = 'out';*/
+console.log("right")
+
+	});
+
+	Hammer(document.getElementById('cont')).on('swipeleft', function(e) {
+/*			$('.slide-in').toggleClass('on');
+			$('#main-container').toggleClass('on');
+			outIn = 'in';*/
+console.log("left")
+	});
+
+
+	function runAnimation() {
+
+		if(outIn == 'out') {
+
+			$('.slide-in').toggleClass('on');
+			$('#main-container').toggleClass('on');
+			outIn = 'in';
+
+		} else if(outIn == 'in') {
+
+			$('.slide-in').toggleClass('on');
+			$('#main-container').toggleClass('on');
+			outIn = 'out';
+
+		}
+
+	}
+
+/*
+	$('.menu-icon')[0].addEventListener('touchend', function(e) {
+		$('.slide-in').toggleClass('on');
+		$('#main-container').toggleClass('on');
+	});
+
+	$('.menu-icon').click(function() {
+		$('.slide-in').toggleClass('on');
+		$('#main-container').toggleClass('on');
+	});
+*/
+
+
+
+});
